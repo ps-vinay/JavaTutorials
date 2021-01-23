8cc7e05870061ec25efdc31fa235e645078cdec7
ps_vinay <vinaysravan93@gmail.com>
2021-01-23 19:12:31 +0530

    initial java practised codes

---
+++ src/com/javatutorial/TypesAndVariables.java
@@ -0,0 +1,14 @@
+package com.javatutorial;
+
+public class TypesAndVariables {
+   public static void main(String[] args) {
+	
+	 
+	 String hello = "vinay";
+	 String name = "sravan";
+	 System.out.println(hello + name);
+	
+			   
+			   
+}
+}
