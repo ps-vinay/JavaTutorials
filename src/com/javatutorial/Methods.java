0bf5665dcf1a624f219fcd1fd4a40d44ee34ef59
ps_vinay <vinaysravan93@gmail.com>
2021-01-24 00:02:23 +0530

    JavaTutorialsMethods

--- src/com/javatutorial/Methods.java
+++ src/com/javatutorial/Methods.java
@@ -3,10 +3,14 @@ package com.javatutorial;
 public class Methods {
     public static void main(String[] args) {
 		sayHelloworld();
-		sayHelloworld();
-		sayHelloworld();
+		
+		sayHelloTo("charlie");
+		sayHelloTo("john");
+	
 	}
-    
+    static void sayHelloTo(String name) {
+    	System.out.println("Hello, " + name);
+    }
     static void sayHelloworld() {
     	System.out.println("Hello, world!");
     }
