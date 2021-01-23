8ec6ee5e49a864580c2f63e4c1f880175b1dcf00
ps_vinay <vinaysravan93@gmail.com>
2021-01-23 23:33:32 +0530

    JavaTutorialmethods2

--- src/com/javatutorial/Methods.java
+++ src/com/javatutorial/Methods.java
@@ -3,6 +3,8 @@ package com.javatutorial;
 public class Methods {
     public static void main(String[] args) {
 		sayHelloworld();
+		sayHelloworld();
+		sayHelloworld();
 	}
     
     static void sayHelloworld() {
